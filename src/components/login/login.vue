d16a32f6a5092720ff53b659e04f1ae74d853ca5
cyj <183417551@qq.com>
2018-11-26 21:10:00 +0800

    'login'

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,76 @@
 <template>
-    
+    <div class="login-drap">
+<el-form class="login-form" label-position="top" label-width="80px"
+:model="formData">
+  <el-form-item label="名称">
+    <el-input v-model="formData.username"></el-input>
+  </el-form-item>
+  <el-form-item label="活动区域">
+    <el-input v-model="formData.password"></el-input>
+  </el-form-item>
+  <el-button type="primary" class="login-btn" @click.prevent="handleLogin()">主要按钮</el-button>
+</el-form>
+
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            formData:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods: {
+        // 在异步操作的外面获取到异步操作里面的结果
+       async handleLogin() {
+           const res = await this.$http.post('login',this.formData)
+                    
+                        console.log(res);
+                        const {
+                            meta:{status,msg},
+                            data
+                        } = res.data
+                        if(status===200){
+                            // 0 保存用户token
+                            // 将来在home中渲染检查
+                            const token = localStorage.setItem('token',data.token)
+                            // 1跳转到首页
+                        this.$router.push({name:'home'})
+                        // 2提示登陆成功
+                        this.$message.success(msg)
+                        
+                        } else {
+                            // 3登陆失败,提示用户
+                            this.$message.warning(msg)
+                        }
+                        
+                        
+                    }
+        }
+    }
 
-}
 </script>
 
 <style>
+    .login-drap {
+       height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+    }
+    .login-drap .login-form {
+        width: 400px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 30px;
+    }
+    .login-drap .login-btn {
+        width: 100%;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import MyHttpServer from '@/plugins/http.js'
+
+import router from './router'
+import '@/assets/css/index.css'
 
 Vue.use(ElementUI)
+Vue.use(MyHttpServer)
 
 Vue.config.productionTip = false
 
